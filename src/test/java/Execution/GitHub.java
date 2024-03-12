4577f6b8c17f321a5f50cb95f7903907aa6ea36b
Himanshu@Dalal <himanshuapachertr160@gmail.com>
2024-03-12 11:20:45 +0530

    Adding class

---
+++ src/test/java/Execution/GitHub.java
@@ -0,0 +1,11 @@
+package Execution;
+
+public class GitHub 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Adding class for to understand Git merge operation");
+		
+	}
+
+}
